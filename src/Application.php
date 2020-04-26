11f35d220ad38ddbcd167c133dd5898f9c145560
Serghei Iakovlev <egrep@protonmail.ch>
2020-04-26 13:55:12 +0300

    Provide ability to get Git version

--- src/Application.php
+++ src/Application.php
@@ -13,6 +13,8 @@ namespace Ppcre;
 
 class Application
 {
+    const GIT_VERSION = '$Id$';
+
     private $argv = [];
     private $argc = 0;
 
@@ -22,8 +24,18 @@ class Application
         $this->argc = \count($argv);
     }
 
+    public function version()
+    {
+        echo self::GIT_VERSION, PHP_EOL;
+    }
+
     public function say()
     {
+        if (isset($this->argv[1]) && $this->argv[1] === '--version') {
+            $this->version();
+            return;
+        }
+
         printf('There were %d arguments passed to PHP:' . PHP_EOL, $this->argc);
         print_r($this->argv);
     }
